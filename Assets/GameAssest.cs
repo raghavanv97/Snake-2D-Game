7c2728d548da6a1f32d7645741e4110c7be48a50
vijayraghavan <chennai1997>
2020-05-22 19:12:00 +0530

    first commit

---
+++ Assets/GameAssest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameAssest : MonoBehaviour
+{
+
+    [SerializeField] public Sprite apple;
+    [SerializeField] public Sprite snakeBody;
+
+    public static GameAssest i;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        i = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
